d6cc684b0db600384e9c5fef165e149b8a83128e
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-11-01 22:00:45 +0200

    Add Commander Options

--- README.md
+++ README.md
@@ -4,9 +4,22 @@ A script to dump Firestore documents for testing.
 
 ## Usage
 
-```shell
-node . --help                    # help
-node . collections               # list available collections
-node . dump [collections...]     # dump documents to collection(s)
-node . clear [collections...]    # clear dumped documents from collection(s)
+```text
+Usage: dumper [options] [command]
+
+Options:
+  -c, --count <number>                number of documents to dump (default: 10)
+  -s, --staff-id <number>             length of staff account's id (default: 6)
+  -u, --user-id <number>              length of user's national id (default: 14)
+  -p, --user-id-prefix <string>       prefix for user's national id (default: "00")
+  -t, --transaction-user-id <string>  user's national id used for transactions
+  -a, --amount <number>               transactions' amount bounds (default: 100)
+  -n, --no-negative                   make zero a lower bound for transactions' amount
+  -y, --start-year <number>           transactions' start year (default: 2018)
+  -h, --help                          output usage information
+
+Commands:
+  collections                         list available collections
+  dump [collections...]               dump documents to collection(s)
+  clear [collections...]              clear dumped documents from collection(s)
 ```
--- dumper.js
+++ dumper.js
@@ -6,6 +6,14 @@ admin.initializeApp({
 });
 
 // Dumpers
+const collectionsNames = [
+  "users",
+  "staff",
+  "buses",
+  "transactions",
+  "feedbacks"
+];
+
 const UsersDumper = require("./dumpers/users");
 const BusesDumper = require("./dumpers/buses");
 const StaffDumper = require("./dumpers/staff");
@@ -14,13 +22,6 @@ const FeedbacksDumper = require("./dumpers/feedbacks");
 
 // Process args
 const program = require("commander");
-const collectionsNames = [
-  "users",
-  "staff",
-  "buses",
-  "transactions",
-  "feedbacks"
-];
 
 program
   .command("collections")
@@ -38,34 +39,34 @@ program
     const collection = mapCollections(collections);
     if (collection.users) {
       UsersDumper.dump({
-        count: 5,
-        idLength: 14,
-        idPrefix: "00"
+        count: program.count,
+        idLength: program.userId,
+        idPrefix: program.userIdPrefix
       });
     }
     if (collection.buses) {
       BusesDumper.dump({
-        count: 5
+        count: program.count
       });
     }
     if (collection.staff) {
       await StaffDumper.dump({
-        count: 5,
-        idLength: 6
+        count: program.count,
+        idLength: program.staffId
       });
     }
     if (collection.transactions) {
       await TransactionsDumper.dump({
-        count: 5,
-        amountRange: 100,
-        allowNegative: true,
-        timestampStart: new Date(2019, 0, 1)
-        // userNationalId: "id_here"
+        count: program.count,
+        amountRange: program.amount,
+        allowNegative: program.negative,
+        timestampStart: new Date(program.startYear, 0, 1),
+        userNationalId: program.transactionUserId
       });
     }
     if (collection.feedbacks) {
       FeedbacksDumper.dump({
-        count: 5
+        count: program.count
       });
     }
   });
@@ -82,7 +83,16 @@ program
     if (collection.feedbacks) FeedbacksDumper.clear();
   });
 
-program.parse(process.argv);
+program
+  .option("-c, --count <number>", "number of documents to dump", parseInt, 10)
+  .option("-s, --staff-id <number>", "length of staff account's id", parseInt, 6)
+  .option("-u, --user-id <number>", "length of user's national id", parseInt, 14)
+  .option("-p, --user-id-prefix <string>", "prefix for user's national id", "00")
+  .option("-t, --transaction-user-id <string>", "user's national id used for transactions")
+  .option("-a, --amount <number>", "transactions' amount bounds", parseInt, 100)
+  .option("-n, --no-negative", "make zero a lower bound for transactions' amount")
+  .option("-y, --start-year <number>", "transactions' start year", parseInt, 2018)
+  .parse(process.argv);
 
 function mapCollections(collections) {
   collections = [...collections];
